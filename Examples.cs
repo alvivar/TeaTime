9442498a0bc67655e2d6fb7c6296c9f22b7197a2
alvivar <andresalvivar@gmail.com>
2014-12-28 23:08:27 -0600

    Typo.

--- Examples.cs
+++ Examples.cs
@@ -22,8 +22,8 @@ this.ttAppend(2, () => Debug.Log("SomeQueue " + Time.time));  // Prints 5
 void Update() 
 {
     this.ttAppend("LockedQueue", 3, () => Debug.Log("LockedQueue " + Time.time));
-	this.ttAppend("LockedQueue", 3, () => Debug.Log("LockedQueue " + Time.time));
-	this.ttLock(); // 'LockedQueue' will run as a safe 2 step timer.
+    this.ttAppend("LockedQueue", 3, () => Debug.Log("LockedQueue " + Time.time));
+    this.ttLock(); // 'LockedQueue' will run as a safe 2 step timer.
 }
 
 // ttNow
