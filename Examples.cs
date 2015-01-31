ba181e86389d9693f25a16da511815ebdc26746f
alvivar <andresalvivar@gmail.com>
2015-01-31 01:47:56 -0600

    Examples updated to v0.5.4 alpha, and some minor improvements.

--- Examples.cs
+++ Examples.cs
@@ -48,16 +48,16 @@ public class Examples : MonoBehaviour
         // without over-appending, they just keep repeating themselves in order.
 
 
-        // 'ttLoop' executes his callback frame by frame for all his duration (like
-        // some sort of controlled update). It requires 'ttHandler' to work, where you can
-        // access special properties, like 't', a custom delta for interpolations.
+        // 'ttLoop' executes his callback frame by frame for all his duration. It
+        // requires 'ttHandler' to work, where you can access special properties, like a
+        // custom delta for interpolations during the loop duration.
 
-        // 'ttAdd' and 'ttLoop' can be mixed. In this example, a 3 seconds 'ttLoop'
-        // will run using Lerp and 't' to change a color, then a 'ttAdd' without time (0)
-        // marks the loop end. After that the same but backwards.
+        // 'ttAdd' and 'ttLoop' can be mixed. In this example, a 3 seconds 'ttLoop' will
+        // run using Lerp and the custom 'deltaTime' to change a color, then a 'ttAdd'
+        // without time (0) marks the loop end. After that the same but backwards.
         this.ttLoop("Background change", 3, delegate(ttHandler loop)
         {
-            Camera.main.backgroundColor = Color.Lerp(Color.white, Color.black, loop.t);
+            Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor, Color.black, loop.deltaTime);
         })
         .ttAdd(() =>
         {
@@ -65,7 +65,7 @@ public class Examples : MonoBehaviour
         })
         .ttLoop(3, delegate(ttHandler loop)
         {
-            Camera.main.backgroundColor = Color.Lerp(Color.black, Color.white, loop.t);
+            Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor, Color.white, loop.deltaTime);
         })
         .ttAdd(() =>
         {
@@ -77,13 +77,13 @@ public class Examples : MonoBehaviour
         // You can also use 'ttHandler' in a normal 'ttAdd' for extra features. In this
         // example we are using 'WaitFor(' to wait for a YieldInstruction (e.g DOTween,
         // WaitForSeconds) after the callback is done and before the next queued callback.
-        this.ttAdd("DOTween example", delegate(ttHandler tt)
+        this.ttAdd("DOTween example", delegate(ttHandler t)
         {
             Sequence myTween = DOTween.Sequence();
             myTween.Append(transform.DOMoveX(5, 2.5f));
             myTween.Append(transform.DOMoveX(-5, 2.5f));
 
-            tt.WaitFor(myTween.WaitForCompletion());
+            t.WaitFor(myTween.WaitForCompletion());
         })
         .ttAdd(() =>
         {
@@ -95,10 +95,10 @@ public class Examples : MonoBehaviour
         // If you call 'ttLoop' without time (or negative) the loop will be infinite. In
         // this case you can use 'timeSinceStart' and 'Break(' from ttHandler to control
         // the loop.
-        this.ttLoop("Timed by break", delegate(ttHandler tt)
+        this.ttLoop("Timed by break", delegate(ttHandler t)
         {
-            if (tt.timeSinceStart > 2)
-                tt.Break();
+            if (t.timeSinceStart > 2)
+                t.Break();
         })
         .ttAdd(() =>
         {
@@ -112,10 +112,11 @@ public class Examples : MonoBehaviour
         // - Queues are unique to his MonoBehaviour (this is an extension after all)
         // - Naming your queue is recommended if you want to use more than one queue with safety
         // - You can use a YieldInstruction instead of time (i.e. WaitForEndOfFrame)
-        // - ttWaitForCompletion ensures a safe run during continuous calls
+        // - ttWaitForCompletion ensures a complete and safe run during continuous calls
         // - ttHandler adds special control features to your callbacks
-        // - waitFor from ttHandler applies only once at the end of the current callback
-        // - ttNow will always ignore queues, it's a sexier Invoke(
+        // - You can create tween-like behaviours mixing loops, ttHandler.deltaTime and Lerp functions
+        // - ttHandler.waitFor applies only once and at the end of the current callback
+        // - ttNow will always ignore queues (it's inmune to ttWaitForCompletion)
         // - Below the sugar, everything runs on coroutines!
     }
 }
