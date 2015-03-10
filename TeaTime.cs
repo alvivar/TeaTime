4ac6a165e25996254f73430340219a9b082b17db
alvivar <andresalvivar@gmail.com>
2015-03-10 03:48:54 -0600

    v0.6.0 alpha docs updated

--- TeaTime.cs
+++ TeaTime.cs
@@ -12,12 +12,12 @@
 // MonoBehaviour using 'this.tt' (and trust the autocomplete).
 
 
-//    this.ttAdd("QueueName", 2, () =>
+//    this.tt("QueueName").ttAdd(2, () =>
 //    {
 //        Debug.Log("2 seconds since QueueName started " + Time.time);
 //    })
 //    .ttLoop(3, delegate(ttHandler loop)
-//    {   
+//    {	
 //        // ttLoop runs frame by frame for all his duration (3s) and his handler have a
 //        // customized delta (loop.deltaTime) that represents the precise loop duration.
 //        Camera.main.backgroundColor 
@@ -28,32 +28,9 @@
 //        Debug.Log("5 seconds since QueueName started " + Time.time);
 //    })
 
+
 // Check out Examples.cs [http://github.com/alvivar/TeaTime/blob/master/Examples.cs]
-// for all the features and explained examples.
-
-
-// **Details to remember**
-// - Execution starts immediately
-// - Queues are unique to his MonoBehaviour (this is an extension after all)
-// - Below the sugar, everything runs on Unity coroutines!
-
-// **Tips**
-// - You can create tween-like behaviours with loops and lerp functions
-// - Always name your queue if you want to use more than one queue with safety 
-// - You can use a YieldInstruction instead of time (e.g. WaitForEndOfFrame)
-
-// **About ttHandler**
-// - ttHandler adds special control features to all your callbacks
-// - ttHandler.deltaTime contains a customized deltaTime that represents the precise loop duration
-// - ttHandler.t contains the completion percentage expressed from 0 to 1 based on the loop duration
-// - ttHandler.waitFor( applies a wait interval once, at the end of the current callback
-
-// **Moar**
-// - tt( changes the current queue, reset it or create an anonymous queue
-// - ttWait() ensures a complete and safe run of the current queue (waits for completion)
-// - TeaTime.Reset( stops and resets queues and instances, TeaTime.ResetAll( resets everything
-
-// And that's it!
+// to learn. (*More to come!*)
 
 
 // Copyright (c) 2014/12/26 andresalvivar@gmail.com
