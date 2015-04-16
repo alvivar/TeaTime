9769891b7d041f199c3900dde2a283a47feca321
alvivar <andresalvivar@gmail.com>
2015-04-15 21:03:40 -0600

    Minor improvements and pdated to v6.2

--- Examples.cs
+++ Examples.cs
@@ -1,61 +1,56 @@
-﻿using DG.Tweening; // If you don't have DOTween, feel free to remove this line and his example.
-using UnityEngine;
+﻿using UnityEngine;
 
 
 public class Examples : MonoBehaviour
 {
     void Start()
     {
-        // TeaTime is a fast & simple queue for timed callbacks, fashioned as a
-        // MonoBehaviour extension set, focused on solving common coroutines patterns in
-        // Unity games.
+        // TeaTime is a fast & simple queue for timed callbacks, fashioned as
+        // a MonoBehaviour extension set, focused on solving common coroutines
+        // patterns in Unity games.
 
-        // Just put 'TeaTime.cs' somewhere in your project and call it inside any
-        // MonoBehaviour using 'this.tt' (and trust the autocomplete).
+        // Just put 'TeaTime.cs' somewhere in your project and call it inside
+        // any MonoBehaviour using 'this.tt' (and trust the autocomplete).
 
 
-
-        // > tt(string queueName = null, bool resetQueue = false)
-        // Creates or changes the current queue.
-        // When used without name the queue will be anonymous and untrackable.
-        // If 'resetQueue = true' the queue will be stopped and overwritten.
-
-        // > ttAdd(float timeDelay, Action callback)
-        // Appends a timed callback into the current queue.
 
         // EXAMPLE
         // Invoke-like Timer
         // 3 seconds callback timer.
+
         this.tt().ttAdd(3, delegate()
         {
             Debug.Log("Invoke-like Timer: 3 seconds have passed " + Time.time);
         });
 
+        // > tt(string queueName = null) Creates or changes the current queue.
+        // When used without queue name the queue will be anonymous and
+        // untrackable.
+
+        // > ttAdd(float timeDelay, Action callback) Appends a timed callback
+        // into the current queue.
 
 
-        // > ttRepeat(int n = 1)
-        // Repeats the current queue n times or infinite (n <= -1).
 
         // EXAMPLE
         // InvokeRepeating-like Timer
         // Repeats the callback every 9s, infinitely.
+
         this.tt().ttAdd(9, delegate()
         {
             Debug.Log("InvokeRepeating-like Timer: 9 seconds have passed (repeating) " + Time.time);
         })
         .ttRepeat(-1); // Repeat infinitely
 
+        // > ttRepeat(int n = 1) Repeats the current queue n times or infinite
+        // (n <= -1).
 
 
-        // > ttLoop(float duration, Action<ttHandler> callback)
-        // Appends into the current queue a callback that runs frame by frame for all his duration.
-
-        // > ttHandler loop; loop.deltaTime
-        // Customized delta that represents the loop duration.
 
         // EXAMPLE
         // Tween-like Color Transition
         // Changes the camera background color to black in 3s, then to gray in 4s, and repeats 2 more times.
+
         this.tt().ttLoop(3, delegate(ttHandler loop)
         {
             Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor, Color.black, loop.deltaTime);
@@ -68,15 +63,20 @@ public class Examples : MonoBehaviour
         {
             Debug.Log("Tween-like Color Transition: End " + Time.time);
         });
+
+        // > ttLoop(float duration, Action<ttHandler> callback) Appends into
+        // the current queue a callback that runs frame by frame for all his
+        // duration, or until ttHandler.Break(). ttLoop always requires
+        // ttHandler to work.
+
+        // ttHandler adds extra functionality to your callbacks. In this
+        // example .deltaTime is a customized delta that represents the exact
+        // the loop duration.
     }
 
 
     void Update()
     {
-        // > ttWait()
-        // Locks the current queue ignoring new appends until all his callbacks are completed (i.e. WaitForCompletion).
-        // Wait for completion. Locks the current queue ignoring new appends until all his callbacks are completed.
-
         // EXAMPLE
         // Wait For Completion Lock
         // Executes a callback and waits 2s during continuous calls without over appending callbacks.
@@ -88,18 +88,23 @@ public class Examples : MonoBehaviour
             })
             .ttAdd(2) // 2s wait
             .ttWait();
-            // ttWait() uses the queue name to set the lock, 
+            // ttWait() uses the queue name to set the lock,
             // allowing tt("queueName") to wait for completion before appending new callbacks.
         }
+
+        // > ttWait() Locks the current queue ignoring new appends until all
+        // his callbacks are completed (i.e. WaitForCompletion). Wait for
+        // completion. Locks the current queue ignoring new appends until all
+        // his callbacks are completed.
 
 
 
         // EXAMPLE
         // Lerp Overwrite
-        // Moves from Vector3.zero to Vector3(5, 5, 5) in 3 seconds, overwriting itselfs (reset) if recalled.
+        // Moves from Vector3.zero to Vector3(5, 5, 5) in 3 seconds, overwriting itselfs (reset) when recalled.
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // The queue is selected, then resetted
+            // The queue is selected, then resetted by using ttReset() after.
             this.tt("LerpOverwrite").ttReset().ttAdd(delegate()
             {
                 transform.position = Vector3.zero;
@@ -113,6 +118,8 @@ public class Examples : MonoBehaviour
                 Debug.Log("Lerp Overwrite: Done " + Time.time);
             });
         }
+
+        // > ttReset() Stops and resets the current queue.
     }
 
 
@@ -127,7 +134,7 @@ public class Examples : MonoBehaviour
 
     // **Tips**
     // - You can create tween-like behaviours with loops and lerp functions
-    // - Always name your queue if you want to use more than one queue with safety 
+    // - Always name your queue if you want to use more than one queue with safety
     // - You can use a YieldInstruction instead of time (e.g. WaitForEndOfFrame)
 
     // **About ttHandler**
