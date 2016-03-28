550e7c861654adfd93dcd93237c60c6cb865c8c4
alvivar <andresalvivar@gmail.com>
2016-03-28 15:35:20 -0600

    More patterns / examples!

--- Examples/TeaTimePatterns.cs
+++ Examples/TeaTimePatterns.cs
@@ -9,25 +9,68 @@ using matnesis.TeaTime;
 
 public class TeaTimePatterns : MonoBehaviour
 {
+    public bool doSomething = false;
+
+
     void Start()
     {
         // @
-        // A simple delay
-        TeaTime simpleDelay = this.tt().Add(1, () =>
+        // A simple 2 seconds delay.
+        TeaTime simpleDelay = this.tt().Add(2, () =>
         {
-            Debug.Log("simpleDelay: One second later, once" + Time.time);
+            Debug.Log("simpleDelay: Two seconds later, just once!" + Time.time);
         });
 
 
         // @
-        // A simple observer
-        TeaTime observeSomething = this.tt().Add(() =>
+        // Something that repeats itself every 3 seconds.
+        TeaTime repeatDelay = this.tt().Add(() =>
         {
-            Debug.Log("observeSomething: Every 3 seconds! " + Time.time);
+            Debug.Log("repeatDelay: Every 3 seconds, repeats forever! " + Time.time);
         })
         .Add(3).Repeat();
 
 
-        // More to come! Soon!
+        // @
+        // A controlled frame by frame loop (update-like) with 1.5 seconds duration!
+        TeaTime updateLike = this.tt().Loop(1.5f, (ttHandler loop) =>
+        {
+            Debug.Log("updateLike: Frame by frame during 1.5 seconds, just once! " + Time.time);
+        });
+
+
+        // @
+        // A simple delay without autoplay.
+        TeaTime somethingForLater = this.tt().Pause().Add(3, () =>
+        {
+            Debug.Log("somethingForLater: Someone called 'somethingForLater.Play()' 3 second ago! " + Time.time);
+        });
+
+        somethingForLater.Play();
+
+
+        // @
+        // A tween-like with before and after setup.
+        TeaTime tweenLike = this.tt().Add(() =>
+        {
+            Debug.Log("tweenLike: Just before the 4 seconds loop! " + Time.time);
+            transform.position = new Vector3(999, 999, 999);
+        })
+        .Loop(4, (ttHandler loop) =>
+        {
+            transform.position = Vector3.Lerp(
+                transform.position,
+                Vector3.zero,
+                loop.deltaTime);
+        })
+        .Add(() =>
+        {
+            Debug.Log("tweenLike: Just after the 4 seconds loop! " + Time.time);
+        });
+
+
+        //
+        // More examples to come! Soon!
+        //
     }
 }
