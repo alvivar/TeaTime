50eca4adb46985261f2e0a82d21d63bec454301c
alvivar <andresalvivar@gmail.com>
2018-02-09 21:07:08 -0600

    Some fixes

--- TeaCup/TeaCupHandler.cs
+++ TeaCup/TeaCupHandler.cs
@@ -1,4 +1,6 @@
 
+// I'm not sure if this is needed.
+
 // @matnesis
 // 2016/11/12 07:31 PM
 
@@ -7,12 +9,6 @@ namespace matnesis.TeaCup
 {
     public class TeaCupHandler
     {
-        public TeaCup self;
-
-
-        public TeaCupHandler(TeaCup self)
-        {
-            this.self = self;
-        }
+        public TeaCupQueue self;
     }
 }
--- TeaCup/TeaCupTest.cs
+++ TeaCup/TeaCupTest.cs
@@ -1,5 +1,5 @@
 
-// I use this to test TeaCup as I experiment with it.
+// I use this to test TeaCup as I create it.
 
 // @matnesis
 // 2016/11/12 07:34 PM
@@ -12,22 +12,24 @@ public class TeaCupTest : MonoBehaviour
 {
     void Start()
     {
-        TeaCup test1 = new TeaCup().Add(1, t =>
-        {
-            Debug.Log("Test1 a " + Time.time);
-        })
-        .Add(0, t =>
-        {
-            Debug.Log("Test1 b " + Time.time);
-        })
-        .Add(2, t =>
-        {
-            Debug.Log("Test1 c " + Time.time);
-        })
-        .Add(1, t =>
-        {
-            Debug.Log("Test1 d " + Time.time);
-        });
+        // TeaCup test1 = new TeaCup().Add(1, t =>
+        // {
+        //     Debug.Log("Test1 a " + Time.time);
+        // })
+        // .Add(0, t =>
+        // {
+        //     Debug.Log("Test1 b " + Time.time);
+        // })
+        // .Add(2, t =>
+        // {
+        //     Debug.Log("Test1 c " + Time.time);
+        //     // t.self.Reverse();
+        // })
+        // .Add(1, t =>
+        // {
+        //     Debug.Log("Test1 d " + Time.time);
+        // });
+        // .Repeat()
         // .Reverse();
 
 
@@ -50,7 +52,43 @@ public class TeaCupTest : MonoBehaviour
         // {
         //     Debug.Log("Time " + Time.time);
         //     Debug.Log("Delta " + Time.deltaTime);
-        // })
+        // });
         // .Repeat();
+
+
+        // Loop
+        var time = 0f;
+        TeaCup test4 = new TeaCup().Loop(0.5f, t =>
+        {
+            time += Time.deltaTime;
+            Debug.Log("1 " + time + " | " + Time.deltaTime + " | " + t.self.lastLoopTime);
+        })
+        .Loop(0.5f, t =>
+        {
+            time += Time.deltaTime;
+            Debug.Log("2 " + time + " | " + Time.deltaTime + " | " + t.self.lastLoopTime);
+        })
+        .Add(1, t => Debug.Log("Time " + time + " " + Time.time))
+        .Reverse();
+
+
+        // Testing if we cant go back between a loop
+        // time = 0;
+        // TeaCup test5 = new TeaCup()
+        //     .Add(1, t => Debug.Log("A Time " + time + " " + Time.time))
+        //     .Loop(0.5f, t =>
+        //     {
+        //         time += Time.deltaTime;
+        //         Debug.Log("1 " + time + " | " + Time.deltaTime + " | " + t.self.lastLoopTime);
+        //     })
+        //     .Loop(3f, t =>
+        //     {
+        //         time += Time.deltaTime;
+        //         Debug.Log("2 " + time + " | " + Time.deltaTime + " | " + t.self.lastLoopTime);
+
+        //         if (t.self.lastLoopTime > 0.5f)
+        //             t.self.isReversed = true;
+        //     })
+        //     .Add(1, t => Debug.Log("B Time " + time + " " + Time.time));
     }
 }
