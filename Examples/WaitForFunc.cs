8a63b0c902c1dadf5e6be71bbc848b7d03108601
Andrés Villalobos <andresalvivar@gmail.com>
2021-02-17 01:35:33 -0600

    New example!

---
+++ Examples/WaitForFunc.cs
@@ -0,0 +1,43 @@
+﻿// Wait!
+
+using UnityEngine;
+
+public class WaitForFunc : MonoBehaviour
+{
+    public bool something = false;
+
+    void Start()
+    {
+        TeaTime untilTrueFunc = this.tt()
+            .Wait(() => something, 0.1f)
+            .Add(() =>
+            {
+                Debug.Log($"Prelude at {Time.time}");
+            })
+            .Loop(0.1f, t =>
+            {
+                Debug.Log($"Action! at {Time.time}");
+            });
+
+        // Both are equivalent. Wait( is syntactic sugar.
+
+        TeaTime waitIsSyntacticSugar = this.tt()
+            .Loop((ttHandler t) =>
+            {
+                if (something)
+                    t.Break();
+
+                t.Wait(0.1f);
+            })
+            .Add(() =>
+            {
+                Debug.Log($"Prelude at {Time.time}");
+            })
+            .Loop(0.1f, t =>
+            {
+                Debug.Log($"Action! at {Time.time}");
+            });
+    }
+}
+
+// 2021.02.17 12.52 am
