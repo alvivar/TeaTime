73c6990907c41a788a0b122b8d5a67a0c092c7ba
Andres Villalobos <andresalvivar@gmail.com>
2026-02-07 04:02:37 -0600

    Fixing Bounce, and adding BounceOut.

--- Tools/Easef.cs
+++ Tools/Easef.cs
@@ -95,7 +95,29 @@ public static class Easef
 
     public static float Bounce(float t)
     {
-        return Mathf.Abs(Mathf.Sin(6.28f * (t + 1f) * (t + 1f)) * (1f - t));
+        return 1f - BounceOut(1f - t);
+    }
+
+    public static float BounceOut(float t)
+    {
+        const float n1 = 7.5625f;
+        const float d1 = 2.75f;
+
+        if (t < 1f / d1)
+            return n1 * t * t;
+        if (t < 2f / d1)
+        {
+            t -= 1.5f / d1;
+            return n1 * t * t + 0.75f;
+        }
+        if (t < 2.5f / d1)
+        {
+            t -= 2.25f / d1;
+            return n1 * t * t + 0.9375f;
+        }
+
+        t -= 2.625f / d1;
+        return n1 * t * t + 0.984375f;
     }
 }
 
